27af1cb6a035ad5eb424d4a2c80749ff662222ff
JamieIsHigh <113109707+JamieIsHigh@users.noreply.github.com>
2023-04-01 21:07:30 +0200

    done menu

--- app/src/main/java/com/example/algit/Oceny.kt
+++ app/src/main/java/com/example/algit/Oceny.kt
@@ -1,11 +1,35 @@
 package com.example.algit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class Oceny : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oceny)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val k = intent.extras
+        if (item.itemId == R.id.infor && k != null) {
+            val nowe = Intent(this, info::class.java)
+            nowe.putExtras(k)
+            startActivity(nowe)
+            return true
+        } else if (item.itemId == R.id.ocen && k != null) {
+            val nowe = Intent(this, Oceny::class.java)
+            nowe.putExtras(k)
+            startActivity(nowe)
+            return true
+        }
+        return false
+    }
+}
 }
--- app/src/main/java/com/example/algit/info.kt
+++ app/src/main/java/com/example/algit/info.kt
@@ -1,8 +1,10 @@
 package com.example.algit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 
 class info : AppCompatActivity() {
@@ -18,7 +20,22 @@ class info : AppCompatActivity() {
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.lista_dan_menu, menu)
+        menuInflater.inflate(R.menu.options, menu)
         return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val k = intent.extras
+        if (item.itemId == R.id.infor && k != null) {
+            val nowe = Intent(this, info::class.java)
+            nowe.putExtras(k)
+            startActivity(nowe)
+            return true
+        } else if (item.itemId == R.id.ocen && k != null) {
+            val nowe = Intent(this, Oceny::class.java)
+            nowe.putExtras(k)
+            startActivity(nowe)
+            return true
+        }
+        return false
     }
 }
